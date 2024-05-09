07aec094cc969a63f90547d9fc39c5fdb720940b
leemj0602 <mjlee2605@gmail.com>
2024-05-09 13:56:43 +0800

    traditional api method

---
+++ api/traditional_api_call.php
@@ -0,0 +1,42 @@
+<?php
+require_once '..\..\wp-content\plugins\civicrm\civicrm\civicrm.config.php';
+require_once '..\..\wp-content\plugins\civicrm\civicrm\CRM\Core\Config.php';
+
+$query = array();
+if (strpos($_SERVER['CONTENT_TYPE'], 'application/json') !== false) {
+    $query = json_decode(file_get_contents("php://input"), true);
+} else {
+    $query = $_POST;
+}
+
+// Specific params variables
+$entity = $query["entity"];
+$action = $query["action"];
+$select = $query['select'] ?? array();
+$where = $query['where'] ?? array();
+$order = $query['order'] ?? array();
+$values = $query['values'] ?? array();
+
+// Initializing default params value
+$params = array(
+    'checkPermissions' => FALSE
+);
+
+// Conditional checking
+$params['limit'] = $query['limit'] ?? null;
+if (is_array($select) && !empty($select)) $params['select'] = $select;
+if (is_array($where) && !empty($where)) $params['where'] = $where;
+if (is_array($order) && !empty($order)) {
+    $orderBy = array();
+    // $order = [[id, ASC], [createdAt, ASC]]
+    foreach($order as $o) {
+        $orderBy[$o[0]] = $o[1];
+    }
+    $params['orderBy'] = $orderBy;
+}
+
+if (is_array($values) && !empty($values)) $params['values'] = $values;
+
+$activityContacts = civicrm_api4($entity, $action, $params);
+
+echo json_encode($activityContacts);
